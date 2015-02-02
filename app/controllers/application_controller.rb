939aa478b0e8908c49aec9cd0ab1a63eb3bd31ac
Nicole <nfelch121394@gmail.com>
2015-02-01 19:53:53 -0500

    Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include ApplicationHelper
+  
+  def authenticate
+    unless is_nicole?
+      redirect_to posts_path, alert: "You are not authorized to post on Nicole's Blog!"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
-
+  def is_nicole?
+    if current_user && current_user.email == "nfelch121394@gmail.com"
+      true
+    else
+      false
+    end
+  end
 end
