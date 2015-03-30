c9f43d645c08dfa488460ff46730aa4e07120887
Nicole <nfelch121394@gmail.com>
2015-03-30 12:34:43 -0400

    Fix if no instagram is present

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,5 +4,3 @@ Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_ID']
   config.client_secret = ENV['INSTAGRAM_SECRET']
 end
-
-CALLBACK_URL = "http://localhost:3000/insta_sessions/callback"
