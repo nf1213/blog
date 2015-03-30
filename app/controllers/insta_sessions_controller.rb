ff4590c724338a4d9ffad8bc16d7ef04566dd27d
Nicole <nfelch121394@gmail.com>
2015-03-29 20:51:26 -0400

    change instagram callback url

--- app/controllers/insta_sessions_controller.rb
+++ app/controllers/insta_sessions_controller.rb
@@ -1,10 +1,10 @@
 class InstaSessionsController < ApplicationController
   def connect
-    redirect_to Instagram.authorize_url(redirect_uri: CALLBACK_URL)
+    redirect_to Instagram.authorize_url(redirect_uri: ENV['CALLBACK_URL'])
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    response = Instagram.get_access_token(params[:code], :redirect_uri => ENV['CALLBACK_URL'])
     session[:access_token] = response.access_token
     redirect_to posts_path
   end
